d4ed338fab48259a0f7f3579e02a02f40b1b376c
Mohamad ALayouni <Ayounimoe.visionbios@gmail.com>
2026-08-17 14:42:07 -0700

    Initial Commit

---
+++ ProjectMan/ProjectManApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManApp.swift
+//  ProjectMan
+//
+//  Created by Mohamad Alayouni on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
